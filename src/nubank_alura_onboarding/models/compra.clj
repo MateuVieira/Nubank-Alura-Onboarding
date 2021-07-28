1e6b4e3b46ed110ce9bf57dc8277061de4f66804
Mateus Vieira <mateus.vieira@mateus.vieira>
2021-07-28 16:30:57 -0300

    Update -  creating the cria-mock-lista-de-compras function first version, only create a number of the same purchecases

--- src/nubank_alura_onboarding/models/compra.clj
+++ src/nubank_alura_onboarding/models/compra.clj
@@ -22,3 +22,20 @@
 
 ;(println "Teste campra:")
 ;(map println (cria-nova-compra "29/07", 100, "loja", "saude"))
+
+(defn cria-mock-lista-de-compras
+  "Esta função irá criar um mock de uma lista de compras
+  com um número n de transações.
+
+  n => Int"
+  ([numero]
+   (cria-mock-lista-de-compras numero []))
+  ([numero lista-de-compras]
+   (if (not (< numero 0))
+     (do
+       ;(println "Numero:" numero "Count:" (count lista-de-compras) "Lista de compras:" lista-de-compras )
+       (recur (dec numero) (conj lista-de-compras (cria-nova-compra "29/07", 100, "loja", "saude"))))
+     lista-de-compras)
+   ))
+
+(println (cria-mock-lista-de-compras 10))
