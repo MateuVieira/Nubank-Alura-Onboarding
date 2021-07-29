002700e5cbd64d5df199e0a23e17e18f9d23e538
mateus.vieira <mateus17v@gmail.com>
2021-07-29 15:18:36 -0300

    Update - creating new functions to handle the calc of the month bill

--- src/nubank_alura_onboarding/core.clj
+++ src/nubank_alura_onboarding/core.clj
@@ -2,7 +2,8 @@
   (:require [nubank-alura-onboarding.models.cliente :as model-cliente]
             [nubank-alura-onboarding.models.card :as model-card]
             [nubank-alura-onboarding.models.compra :as model-compra]
-            [clojure.pprint :as pp]))
+            [clojure.pprint :as pp]
+            [clojure.string :as str]))
 
 ;(println "Teste - Cliente:\n" (model-cliente/cria-novo-cliente "Mateus", "0000.0000.0000-00", "teste@nubank.com.br"))
 ;(println "\nTeste - Catão de Crédito:\n" (model-card/cria-novo-cartao "1111-1111-1111-1111", 011, "01/22", 4000))
@@ -30,6 +31,26 @@
              (group-by :categoria)
              (map calc-total-por-categoria))))
 
+(defn pega-mes?
+  [compra mes]
+  (let [data (get compra :data)
+        data-split (str/split data #"/")
+        mes-compra (get data-split 1)]
+    (= mes-compra mes)))
+
+(defn filtra-por-mes
+  [mes]
+  (fn [lista-de-compras] (pega-mes? lista-de-compras mes)))
+
+(defn calculo-de-fatura-do-mes
+  [lista-de-compras mes]
+  (let [filter-by-mes (filter (filtra-por-mes mes) lista-de-compras)
+        valor-das-compras (map :valor filter-by-mes)
+        total (calc-total valor-das-compras )]
+    (println "\nValor total da Fatura do mês" mes "\n->" total)))
+
+; MAIN
 (let [lista-de-compras (model-compra/cria-mock-lista-de-compras 100)]
   (model-compra/print-lista-de-compras lista-de-compras)
-  (total-por-categoria lista-de-compras))
+  (total-por-categoria lista-de-compras)
+  (calculo-de-fatura-do-mes lista-de-compras "11"))
