9b4cad2fae8cbe87ee9f8dc9cdf2e3aa446dbddb
Mateus Vieira de Paula <mateus12v@hotmail.com>
2021-08-12 18:53:17 -0300

    Update - refactorando o código para poder fazer a referência ao cartão de crédito

--- src/nubank_alura_onboarding/models/cliente.clj
+++ src/nubank_alura_onboarding/models/cliente.clj
@@ -1,12 +1,14 @@
 (ns nubank-alura-onboarding.models.cliente
   (:require [schema.core :as s]
+            [nubank-alura-onboarding.models.card :as m-card]
             [nubank-alura-onboarding.models.common :as m-common]))
 
 (s/def Cliente
-  {:cliente/id    java.util.UUID
-   :cliente/nome  s/Str
-   :cliente/cpf   s/Str
-   :cliente/email s/Str})
+  {:cliente/id                      java.util.UUID
+   (s/optional-key :cliente/nome)   s/Str
+   (s/optional-key :cliente/cpf)    s/Str
+   (s/optional-key :cliente/email)  s/Str
+   (s/optional-key :cliente/cartao) m-card/Cartao})
 
 (s/defn cria-novo-cliente :- Cliente
   "Input:
@@ -21,15 +23,17 @@
     :email
    }"
   ([nome :- s/Str, cpf :- s/Str, email :- s/Str]
-   (cria-novo-cliente (m-common/cria-uuid), nome, cpf, email))
+   (cria-novo-cliente (m-common/cria-uuid), nome, cpf, email, (m-card/gera-um-cartao)))
   ([uuid :- java.util.UUID
     nome :- s/Str
     cpf :- s/Str
-    email :- s/Str]
-   {:cliente/id    uuid
-    :cliente/nome  nome
-    :cliente/cpf   cpf
-    :cliente/email email}))
+    email :- s/Str
+    cartao :- m-card/Cartao]
+   {:cliente/id     uuid
+    :cliente/nome   nome
+    :cliente/cpf    cpf
+    :cliente/email  email
+    :cliente/cartao cartao}))
 
 (let [cliente (cria-novo-cliente "Mateus" "0000.0000.0000-00" "teste@nubank.com.br")]
   (println "Teste cria cliente:")
